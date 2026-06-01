db7b622087c111f6fc7298794c058c13b8f1056a
unknown <rhoheejae>
2026-06-02 08:45:18 +0900

    feat: add ArgoCD apps and service configs

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,9 +12,30 @@ provider "kubernetes" {
   config_context = "minikube"
 }
 
+# platform-system 네임스페이스
 resource "kubernetes_namespace_v1" "platform_system" {
   metadata {
     name = "platform-system"
+    labels = {
+      "app.kubernetes.io/managed-by" = "terraform"
+    }
+  }
+}
+
+# data-starrocks 네임스페이스
+resource "kubernetes_namespace_v1" "data_starrocks" {
+  metadata {
+    name = "data-starrocks"
+    labels = {
+      "app.kubernetes.io/managed-by" = "terraform"
+    }
+  }
+}
+
+# cicd 네임스페이스
+resource "kubernetes_namespace_v1" "cicd" {
+  metadata {
+    name = "cicd"
     labels = {
       "app.kubernetes.io/managed-by" = "terraform"
     }
